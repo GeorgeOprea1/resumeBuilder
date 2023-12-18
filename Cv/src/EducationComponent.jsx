36046713fa567793bf254aee24e8d2b3e1e3ecc8
GeorgeOprea <oprea.george14@yahoo.com>
2023-12-18 21:42:12 +0200

    the input sections for education and experience are now dropdown menus

--- Cv/src/EducationComponent.jsx
+++ Cv/src/EducationComponent.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import "./styles/EducationComponent.css";
 import { FaGraduationCap } from "react-icons/fa6";
+import { FaChevronDown } from "react-icons/fa6";
+import { FaChevronUp } from "react-icons/fa";
 
 const EducationComponent = ({ onSave }) => {
   const [school, setSchool] = useState("");
@@ -8,6 +10,11 @@ const EducationComponent = ({ onSave }) => {
   const [startDate, setStartDate] = useState("");
   const [endDate, setEndDate] = useState("");
   const [location, setLocation] = useState("");
+  const [edForm, setEdForm] = useState(false);
+
+  function handleClick() {
+    setEdForm(!edForm);
+  }
 
   const handleSave = () => {
     if (
@@ -29,63 +36,71 @@ const EducationComponent = ({ onSave }) => {
 
   return (
     <div className="input-container">
-      <h1 className="education-component-header">
-        <FaGraduationCap />
-        Education
-      </h1>
-      <div className="education-component-container">
-        <label htmlFor="school">School</label>
-        <input
-          id="school"
-          type="text"
-          placeholder="School"
-          value={school}
-          onChange={(e) => setSchool(e.target.value)}
-        />
-      </div>
-      <div className="education-component-container">
-        <label htmlFor="degree">Degree</label>
-        <input
-          id="degree"
-          type="text"
-          placeholder="Degree"
-          value={degree}
-          onChange={(e) => setDegree(e.target.value)}
-        />
-      </div>
-      <div className="education-component-container">
-        <label htmlFor="start">Start Date</label>
-        <input
-          id="start"
-          type="text"
-          placeholder="Start Date"
-          value={startDate}
-          onChange={(e) => setStartDate(e.target.value)}
-        />
-      </div>
-      <div className="education-component-container">
-        <label htmlFor="ends">End Date</label>
-        <input
-          id="end"
-          type="text"
-          placeholder="End Date"
-          value={endDate}
-          onChange={(e) => setEndDate(e.target.value)}
-        />
-      </div>
-      <div className="education-component-container">
-        <label htmlFor="location">Location</label>
-        <input
-          id={location}
-          type="text"
-          placeholder="Location"
-          value={location}
-          onChange={(e) => setLocation(e.target.value)}
-        />
-      </div>
-      <button id="saveBtn" onClick={handleSave}>
-        Save
+      <button id="education-component-button" onClick={handleClick}>
+        <h1 className="educationH1">
+          <FaGraduationCap /> Education
+        </h1>
+        {edForm ? <FaChevronUp /> : <FaChevronDown />}
       </button>
+      {edForm ? (
+        <>
+          <div className="education-component-container">
+            <label htmlFor="school">School</label>
+            <input
+              id="school"
+              type="text"
+              placeholder="School"
+              value={school}
+              onChange={(e) => setSchool(e.target.value)}
+            />
+          </div>
+          <div className="education-component-container">
+            <label htmlFor="degree">Degree</label>
+            <input
+              id="degree"
+              type="text"
+              placeholder="Degree"
+              value={degree}
+              onChange={(e) => setDegree(e.target.value)}
+            />
+          </div>
+          <div className="education-component-container">
+            <label htmlFor="start">Start Date</label>
+            <input
+              id="start"
+              type="text"
+              placeholder="Start Date"
+              value={startDate}
+              onChange={(e) => setStartDate(e.target.value)}
+            />
+          </div>
+          <div className="education-component-container">
+            <label htmlFor="ends">End Date</label>
+            <input
+              id="end"
+              type="text"
+              placeholder="End Date"
+              value={endDate}
+              onChange={(e) => setEndDate(e.target.value)}
+            />
+          </div>
+          <div className="education-component-container">
+            <label htmlFor="location">Location</label>
+            <input
+              id={location}
+              type="text"
+              placeholder="Location"
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
+            />
+          </div>
+          <button id="saveBtn" onClick={handleSave}>
+            Save
+          </button>
+        </>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
--- Cv/src/ExperienceComponent.jsx
+++ Cv/src/ExperienceComponent.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import "./styles/ExperienceComponent.css";
 import { FaSuitcase } from "react-icons/fa";
+import { FaChevronDown } from "react-icons/fa6";
+import { FaChevronUp } from "react-icons/fa";
 
 const ExperienceComponent = ({ onSave }) => {
   const [company, setCompany] = useState("");
@@ -9,6 +11,11 @@ const ExperienceComponent = ({ onSave }) => {
   const [endExDate, setEndExDate] = useState("");
   const [locationEx, setLocationEx] = useState("");
   const [description, setDescription] = useState("");
+  const [exForm, setExForm] = useState(false);
+
+  function handleClick() {
+    setExForm(!exForm);
+  }
 
   const handleSave = () => {
     if (
@@ -38,74 +45,82 @@ const ExperienceComponent = ({ onSave }) => {
 
   return (
     <div className="input-container">
-      <h1 className="experience-component-header">
-        {" "}
-        <FaSuitcase />
-        Experience
-      </h1>
-      <div className="experience-component-container">
-        <label htmlFor="company">Company</label>
-        <input
-          id="company"
-          type="text"
-          placeholder="Company"
-          value={company}
-          onChange={(e) => setCompany(e.target.value)}
-        />
-      </div>
-      <div className="experience-component-container">
-        <label htmlFor="position">Position </label>
-        <input
-          id="position"
-          type="text"
-          placeholder="Position"
-          value={position}
-          onChange={(e) => setPosition(e.target.value)}
-        />
-      </div>
-      <div className="experience-component-container">
-        <label htmlFor="Start">Start Date</label>
-        <input
-          id="Start"
-          type="text"
-          placeholder="Start Date"
-          value={startExDate}
-          onChange={(e) => setStartExDate(e.target.value)}
-        />
-      </div>
-      <div className="experience-component-container">
-        <label htmlFor="End">End Date</label>
-        <input
-          id="End"
-          type="text"
-          placeholder="End Date"
-          value={endExDate}
-          onChange={(e) => setEndExDate(e.target.value)}
-        />
-      </div>
-      <div className="experience-component-container">
-        <label htmlFor="location">Location</label>
-        <input
-          id="location"
-          type="text"
-          placeholder="Location"
-          value={locationEx}
-          onChange={(e) => setLocationEx(e.target.value)}
-        />
-      </div>
-      <div className="experience-component-container">
-        <label htmlFor="description">Description</label>
-        <textarea
-          id="description"
-          type="text"
-          placeholder="Enter Description"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-        />
-      </div>
-      <button id="SaveBtn" onClick={handleSave}>
-        Save
+      <button id="experience-component-button" onClick={handleClick}>
+        <h1 className="experienceH1">
+          <FaSuitcase />
+          Experience
+        </h1>
+        {exForm ? <FaChevronUp /> : <FaChevronDown />}
       </button>
+      {exForm ? (
+        <>
+          <div className="experience-component-container">
+            <label htmlFor="company">Company</label>
+            <input
+              id="company"
+              type="text"
+              placeholder="Company"
+              value={company}
+              onChange={(e) => setCompany(e.target.value)}
+            />
+          </div>
+          <div className="experience-component-container">
+            <label htmlFor="position">Position </label>
+            <input
+              id="position"
+              type="text"
+              placeholder="Position"
+              value={position}
+              onChange={(e) => setPosition(e.target.value)}
+            />
+          </div>
+          <div className="experience-component-container">
+            <label htmlFor="Start">Start Date</label>
+            <input
+              id="Start"
+              type="text"
+              placeholder="Start Date"
+              value={startExDate}
+              onChange={(e) => setStartExDate(e.target.value)}
+            />
+          </div>
+          <div className="experience-component-container">
+            <label htmlFor="End">End Date</label>
+            <input
+              id="End"
+              type="text"
+              placeholder="End Date"
+              value={endExDate}
+              onChange={(e) => setEndExDate(e.target.value)}
+            />
+          </div>
+          <div className="experience-component-container">
+            <label htmlFor="location">Location</label>
+            <input
+              id="location"
+              type="text"
+              placeholder="Location"
+              value={locationEx}
+              onChange={(e) => setLocationEx(e.target.value)}
+            />
+          </div>
+          <div className="experience-component-container">
+            <label htmlFor="description">Description</label>
+            <textarea
+              id="description"
+              type="text"
+              placeholder="Enter Description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          </div>
+          <button id="SaveBtn" onClick={handleSave}>
+            Save
+          </button>
+        </>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
