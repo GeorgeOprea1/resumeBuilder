e605835ef517d7a20f3ec0d583350ec7d2baf21d
GeorgeOprea <oprea.george14@yahoo.com>
2023-12-18 18:04:28 +0200

    updated design op the app

--- Cv/src/DisplayPersonalInfo.jsx
+++ Cv/src/DisplayPersonalInfo.jsx
@@ -6,21 +6,21 @@ import { FaLocationDot } from "react-icons/fa6";
 const DisplayPersonalInfo = (props) => {
   return (
     <div className="personal-info-container">
-      <div className="name">{props.fullName} </div>
+      <div className="resume-name">{props.fullName} </div>
       <div className="resume-contact">
         {props.email ? (
-          <div>
-            <IoIosMail /> {props.email}
+          <div className="contact-container">
+            <IoIosMail className="icon" /> {props.email}
           </div>
         ) : null}
         {props.phoneNumber ? (
-          <div>
-            <FaPhone /> {props.phoneNumber}
+          <div className="contact-container">
+            <FaPhone className="icon" /> {props.phoneNumber}
           </div>
         ) : null}
         {props.address ? (
-          <div>
-            <FaLocationDot /> {props.address}
+          <div className="contact-container">
+            <FaLocationDot className="icon" /> {props.address}
           </div>
         ) : null}
       </div>
--- Cv/src/PersonalInfoComponent.jsx
+++ Cv/src/PersonalInfoComponent.jsx
@@ -1,30 +1,37 @@
+import "./styles/PersonalInfoComponent.css";
+
 const PersonalInfoComponent = (props) => {
   return (
     <div className="input-container">
-      <h2>Personal Information</h2>
+      <h2 className="personal-info-header">Personal Details</h2>
       <div className="personal-input-container">
         <label htmlFor="fullName">Full Name:</label>
         <input
           type="text"
           id="fullName"
+          placeholder="Full Name"
           value={props.fullName}
           onChange={props.nameChange}
         />
       </div>
-      <div className="personal-input-container">
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          value={props.email}
-          onChange={props.emailChange}
-        />
+      <div>
+        <div className="personal-input-container">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            placeholder="Email"
+            value={props.email}
+            onChange={props.emailChange}
+          />
+        </div>
       </div>
       <div className="personal-input-container">
         <label htmlFor="phoneNumber">Phone Number:</label>
         <input
           type="tel"
           id="phoneNumber"
+          placeholder="Phone Number"
           value={props.phoneNumber}
           onChange={props.phoneChange}
         />
@@ -34,11 +41,14 @@ const PersonalInfoComponent = (props) => {
         <input
           type="text"
           id="address"
+          placeholder="Address"
           value={props.address}
           onChange={props.addressChange}
         />
       </div>
-      <button onClick={props.onDelete}>Delete</button>
+      <button id="personalDelete" onClick={props.onDelete}>
+        Delete
+      </button>
     </div>
   );
 };
